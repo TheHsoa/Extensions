56df48f53bb04dd0597e758223d6f5bbe5b8a3a4
TheHsoa <alexrassk@gmail.com>
2017-02-04 16:52:40 +0700

    Lection 11 slide 14

---
+++ ConsoleWriteArrayExtension/ConsoleWriteArrayExtension/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+//  Задание из лекции 11, сайд 14
+namespace ConsoleWriteArrayExtension
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            var random = new Random(Guid.NewGuid().GetHashCode());
+            var arrayLength = random.Next(10, 100);
+            var array = new int[arrayLength];
+
+            for (var i = 0; i < arrayLength; i++)
+            {
+                array[i] = random.Next(-100, 100);
+            }
+
+            array.WriteIntegerArray();
+
+            Console.ReadKey();
+        }
+    }
+}
