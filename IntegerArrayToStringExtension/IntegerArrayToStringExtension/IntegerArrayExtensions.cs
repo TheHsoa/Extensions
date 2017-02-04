07d1eb7d65bf74cabe348229e6c49a88b2cd16dc
TheHsoa <alexrassk@gmail.com>
2017-02-04 17:04:56 +0700

    lection 11 HW

---
+++ IntegerArrayToStringExtension/IntegerArrayToStringExtension/IntegerArrayExtensions.cs
@@ -0,0 +1,10 @@
+﻿namespace IntegerArrayToStringExtension
+{
+    public static class IntegerArrayExtensions
+    {
+        public static string IntegerArrayToStringExtension(this int[] array, string separator)
+        {
+            return string.Join(separator, array);
+        }
+    }
+}
